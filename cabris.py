582505fbb55e0b85e558acd88190fb298615c895
JonMakko <danila-bibik2017@yandex.ru>
2022-07-25 20:34:46 +0700

    complete

---
+++ cabris.py
@@ -0,0 +1,36 @@
+from dadata import Dadata
+from work_with_sqlite import token, url, language
+
+
+dadata = Dadata(token)
+
+def desired_location(word):
+    result = dadata.suggest(url, word, language=language)
+    return result
+
+def address_coordinates(address):
+    result = dadata.suggest(url, address, language=language, count=1)
+    return('широта ' + str(result[0]['data']['geo_lat']) + ' долгота ' + str(result[0]['data']['geo_lon']))
+
+
+if __name__ == "__main__":
+    print('-----------------------------------------------------------')
+    print('Добро пожаловать!')
+    while True:
+        print('-----------------------------------------------------------')
+        print('Введите какое примерно место хотите найти\nЕсли хотите закрыть ПО напишите "Закрыть"')
+        print('-----------------------------------------------------------')
+        location = input()
+        if location == 'Закрыть':
+            print('-----------------------------------------------------------')
+            print("До новых встреч!")
+            print('-----------------------------------------------------------')
+            break
+        result = desired_location(location)
+        for i in range(len(result)):
+            print(result[i]['value'])
+        print('-----------------------------------------------------------')
+        print("Введите адресс, чтобы узнать координаты")
+        print('-----------------------------------------------------------')
+        address = input()
+        print(address_coordinates(address))
---
+++ work_with_sqlite.py
@@ -0,0 +1,25 @@
+import os
+import sqlite3
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN', default='dly_proverki_soydet_i_takou')
+ADDRESS = os.getenv('ADDRESS', default='address')
+LANGUAGE = 'ru'
+
+connection = sqlite3.connect('sqlite3.db')
+cursor = connection.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS sqlite3
+              (Token TEXT, URL TEXT, Language TEXT)''')
+
+cursor.execute(
+    f"INSERT INTO sqlite3 VALUES ('{TOKEN}', '{ADDRESS}', '{LANGUAGE}')")
+
+cursor.execute("SELECT * FROM sqlite3")
+token, url, language = (cursor.fetchone())
+
+connection.commit()
+connection.close()
